9ff26b2e4549addee385c4401821b8b78ceb33e6
Andris Pavenis <andris.pavenis@iki.fi>
2014-07-16 21:13:44 +0300

    [DJGPP] mk-djcross-gcc.sh: Use GIT tags for gcc-4.9.1 release
    Tags are not yet created at time of this commit

--- djgpp/mk-djcross-gcc.sh
+++ djgpp/mk-djcross-gcc.sh
@@ -15,9 +15,9 @@ basever=$(cat ../gcc/BASE-VER)
 datestamp=$(cat ../gcc/DATESTAMP)
 devphase=$(cat ../gcc/DEV-PHASE)
 
-upstream=gcc-4_9-branch
-dj_branch=gcc_4_9_djgpp
-djn_branch=gcc_4_9_djgpp_native
+upstream=tags/4.9.1
+dj_branch=tags/4.9.1-djgpp
+djn_branch=tags/4.9.1-djgpp-native
 
 sver2=$(echo $basever | sed -e 's:\.:_:2g' | sed 's:_.*$::')
 
