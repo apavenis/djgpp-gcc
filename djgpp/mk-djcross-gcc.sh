a8d29046433f30a5ca1da62ea34a1c038ce2482f
Andris Pavenis <andris.pavenis@iki.fi>
2014-10-30 18:13:54 +0200

    [DJGPP] Update for DJGPP release of GCC-4.9.2

--- djgpp/mk-djcross-gcc.sh
+++ djgpp/mk-djcross-gcc.sh
@@ -15,9 +15,9 @@ basever=$(cat ../gcc/BASE-VER)
 datestamp=$(cat ../gcc/DATESTAMP)
 devphase=$(cat ../gcc/DEV-PHASE)
 
-upstream=gcc-4_9-branch
-dj_branch=gcc_4_9_djgpp
-djn_branch=gcc_4_9_djgpp_native
+upstream=tags/4.9.2
+dj_branch=tags/4.9.2-djgpp-1.0
+djn_branch=tags/4.9.2-djgpp-native-1.0
 
 sver2=$(echo $basever | sed -e 's:\.:_:2g' | sed 's:_.*$::')
 
