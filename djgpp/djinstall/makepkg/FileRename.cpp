b429f2225b43d5212f2dc9fab17ec52c076baff1
Andris Pavēnis <andris.pavenis@fmi.fi>
2022-01-26 18:16:35 +0200

    [djgpp] djinstall/makepkg: update

--- djgpp/djinstall/makepkg/FileRename.cpp
+++ djgpp/djinstall/makepkg/FileRename.cpp
@@ -6,8 +6,8 @@ const std::vector<std::pair<std::filesystem::path, std::filesystem::path> > PkgB
     {"lib/libstdcxx.a-gdb.py", "libstdcxx_a-gdb-py"},
     {"lib/libstdc++fs.a", "libstdcfs.a"},
     {"lib/libstdc++fs.la", "libstdcfs.la"},
+    {"libexec/gcc/djgpp/11/g++-mapper-server.exe", "gxx-mapper-server.exe"},
     {"bin/g++.exe", "gpp.exe"},
-    {"share/man/man1/g++.1", "gpp.1"},
-    {"lib/gcc/djgpp/10/adainclude/standard.ads.h", "standard_ads.h"}
+    {"share/man/man1/g++.1", "gpp.1"}
 };
 
